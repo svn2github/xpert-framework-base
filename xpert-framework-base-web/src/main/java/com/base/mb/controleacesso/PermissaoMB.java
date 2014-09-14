27136da4c20a2a0fb2e12bd5e032eb47ac60c83b
ayslanms@gmail.com <ayslanms@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2014-09-14 19:11:29 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework-base/trunk@462 e489019e-bc00-2c72-aadb-f79410f744ba

--- xpert-framework-base-web/src/main/java/com/base/mb/controleacesso/PermissaoMB.java
+++ xpert-framework-base-web/src/main/java/com/base/mb/controleacesso/PermissaoMB.java
@@ -1,11 +1,14 @@
 package com.base.mb.controleacesso;
 
-
 import com.base.bo.controleacesso.PermissaoBO;
 import com.base.modelo.controleacesso.Permissao;
 import java.io.Serializable;
 import com.xpert.core.crud.AbstractBaseBean;
 import com.xpert.core.crud.AbstractBusinessObject;
+import com.xpert.faces.utils.FacesMessageUtils;
+import com.xpert.faces.utils.FacesUtils;
+import com.xpert.i18n.XpertResourceBundle;
+import com.xpert.persistence.exception.DeleteException;
 import com.xpert.persistence.query.Restriction;
 import java.util.List;
 import javax.ejb.EJB;
@@ -37,6 +40,20 @@ public class PermissaoMB extends AbstractBaseBean<Permissao> implements Serializ
     public List<Restriction> getDataModelRestrictions() {
         return null;
     }
-    
-    
+
+    public void deleteArvore() {
+        try {
+            Object id = getId();
+            if (getId() != null) {
+                getBO().delete(id);
+                FacesMessageUtils.sucess();
+                id = null;
+                //recarregar tree
+                PerfilMB perfilMB = FacesUtils.getBeanByEl("#{perfilMB}");
+                perfilMB.carregarTree();
+            }
+        } catch (DeleteException ex) {
+            FacesMessageUtils.error(XpertResourceBundle.get("objectCannotBeDeleted"));
+        }
+    }
 }
