db9dc259732b78d0b371c7ab22e7ee93ef753cda
ayslanms@gmail.com <ayslanms@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2014-10-31 16:28:22 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework-base/trunk@504 e489019e-bc00-2c72-aadb-f79410f744ba

--- xpert-framework-base-main/src/main/java/com/base/modelo/controleacesso/Usuario.java
+++ xpert-framework-base-main/src/main/java/com/base/modelo/controleacesso/Usuario.java
@@ -16,7 +16,7 @@ public class Usuario implements Serializable, User {
 
     @Id
     @SequenceGenerator(name = "Usuario", allocationSize = 1, sequenceName = "usuario_id_seq")
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(generator = "Usuario")
     private Long id;
 
     @Temporal(TemporalType.TIMESTAMP)
