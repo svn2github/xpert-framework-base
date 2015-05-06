711d4067c6dfb51b814c5bc7349c0503879a6731
ayslanms@gmail.com <ayslanms@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2015-05-06 21:43:01 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework-base/trunk@653 e489019e-bc00-2c72-aadb-f79410f744ba

--- xpert-framework-base-ejb/src/main/java/com/base/bo/controleacesso/PerfilBO.java
+++ xpert-framework-base-ejb/src/main/java/com/base/bo/controleacesso/PerfilBO.java
@@ -1,8 +1,10 @@
 package com.base.bo.controleacesso;
 
 import com.base.dao.controleacesso.PerfilDAO;
+import com.base.dao.controleacesso.PermissaoDAO;
 import com.base.dao.controleacesso.UsuarioDAO;
 import com.base.modelo.controleacesso.Perfil;
+import com.base.modelo.controleacesso.Permissao;
 import com.base.modelo.controleacesso.Usuario;
 import com.base.util.SessaoUtils;
 import com.xpert.core.crud.AbstractBusinessObject;
@@ -26,6 +28,10 @@ public class PerfilBO extends AbstractBusinessObject<Perfil> {
     private PerfilDAO perfilDAO;
     @EJB
     private UsuarioDAO usuarioDAO;
+    @EJB
+    private PermissaoBO permissaoBO;
+    @EJB
+    private PermissaoDAO permissaoDAO;
 
     @Override
     public BaseDAO getDAO() {
@@ -37,10 +43,39 @@ public class PerfilBO extends AbstractBusinessObject<Perfil> {
         return new UniqueFields().add("descricao");
     }
 
+    /**
+     * metodo para garantir que uma permissao nao seja adicionada sem que o
+     * usuario logado possua ela
+     * 
+     * @param perfil
+     */
+    public void verificarPermissoesPerfil(Perfil perfil) {
+        //para o super usuario isso nao e necessario
+        if (SessaoUtils.getUser().isSuperUsuario() == false) {
+            
+            List<Permissao> permissoesUsuarioLogado = permissaoBO.getPermissoes(SessaoUtils.getUser());
+            List<Permissao> permissoesBanco = new ArrayList<Permissao>();
+            if (perfil.getId() != null) {
+                permissoesBanco = permissaoDAO.getPermissoes(perfil);
+            }
+            //iterar sobre a lista das permissoes selecionadas na tela, caso o usuario nao tenha acesso a ela, ela sera removida
+            List<Permissao> permissoesARemover = new ArrayList<Permissao>();
+            for (Permissao permissao : perfil.getPermissoes()) {
+                //se o usuario nao contem essa permissao e ela nao esta adicionada no banco, remover
+                if (!permissoesUsuarioLogado.contains(permissao) && !permissoesBanco.contains(permissao)) {
+                    permissoesARemover.add(permissao);
+                }
+            }
+            perfil.getPermissoes().removeAll(permissoesARemover);
+
+        }
+    }
+
     @Override
     public void save(Perfil perfil) throws BusinessException {
 
         boolean novo = perfil.getId() == null;
+        verificarPermissoesPerfil(perfil);
         super.save(perfil);
 
         if (novo == true) {
